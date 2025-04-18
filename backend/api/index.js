f56897adc0fb5ed918c2f18026577df802e34b09
Nitin Kant <kantnitin7@gmail.com>
2025-04-18 20:43:17 +0530

    fixed index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,47 +1,38 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "../utils/db.js";
 import userRoute from "../routes/user.route.js";
 import companyRoute from "../routes/company.route.js";
-import jobRoute from "../routes/job.route.js"
-import applicationRoute from "../routes/application.route.js"
-import { createServer } from "http";
-import { parse } from "url";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
 
-
-dotenv.config({});
-const app = express();
+dotenv.config();
 connectDB();
 
-
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors(
-  {
-      origin: ["https://job-quest-frontend.vercel.app"],
-      methods: ["POST", "GET"],
-      credentials: true
-  }
-));
+app.use(
+  cors({
+    origin: ["https://job-quest-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
-app.use("/v1/user",userRoute);
-app.use("/v1/company",companyRoute);
-app.use("/v1/job",jobRoute);
-app.use("/v1/application",applicationRoute);
+app.use("/v1/user", userRoute);
+app.use("/v1/company", companyRoute);
+app.use("/v1/job", jobRoute);
+app.use("/v1/application", applicationRoute);
 
+app.get("/", (req, res) => {
+  res.json("API working ");
+});
 
-app.get("/",(req,res)=>{
-  res.json("Ma ki chooot")
-})
-
-export default function handler(req, res) {
-  const server = createServer(app);
-  const parsedUrl = parse(req.url, true);
-  req.query = parsedUrl.query;
-  return server.emit("request", req, res);
-}
+// 👇👇 This is the only thing Vercel needs for the handler:
+export default app;
