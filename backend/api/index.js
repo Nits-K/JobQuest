37e3858df4ee18084a748bc10bd31c95936be99e
Nitin Kant <kantnitin7@gmail.com>
2025-04-18 20:02:27 +0530

    updated index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from 'dotenv';
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js"
-import applicationRoute from "./routes/application.route.js"
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js"
+import applicationRoute from "../routes/application.route.js"
+import { createServer } from "http";
+import { parse } from "url";
+
+
 dotenv.config({});
 const app = express();
+connectDB();
 
 
 
@@ -24,18 +29,19 @@ app.use(cors(
   }
 ));
 
-const PORT = process.env.PORT || 3000;
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
 
-app.listen(PORT, () => {
-    connectDB();
-  console.log(`server running at ${PORT}`);
-});
-
 app.get("/",(req,res)=>{
   res.json("Ma ki chooot")
 })
+
+export default function handler(req, res) {
+  const server = createServer(app);
+  const parsedUrl = parse(req.url, true);
+  req.query = parsedUrl.query;
+  return server.emit("request", req, res);
+}
