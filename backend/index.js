9c7ca24a95b6b2faa55420b4b58bb91de89cc4fd
Nitin Kant <kantnitin7@gmail.com>
2025-04-18 19:32:39 +0530

    modified configs

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors(
+  {
+      origin: ["https://job-quest-frontend.vercel.app"],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
+
 const PORT = process.env.PORT || 3000;
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
