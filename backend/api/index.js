59899f85f1ed2e207b451ce2c33a8ede09936964
Nitin Kant <kantnitin7@gmail.com>
2025-04-18 20:33:00 +0530

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,7 +29,7 @@ app.use(cors(
   }
 ));
 
-app.use("/api/v1/user",userRoute);
+app.use("/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
